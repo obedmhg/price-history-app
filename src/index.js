c8c339508d9a247dd43e0ffc3d67ea8a7a3e60df
Obed Murillo <obedmhg@gmail.com>
2019-03-05 12:48:45 -0700

    Adding interceptors to axios requests and response, gobally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    request.headers = {'myHeader': 'Testing custome Header'};
+    console.log(request);
+    return request;
+}, error => {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    return response;
+}, error => {
+    return Promise.reject(error);
+});
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
